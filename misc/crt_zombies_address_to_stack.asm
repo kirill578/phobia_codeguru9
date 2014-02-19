b73759771638855e9bb9e717017df6e0a0ef0b50
Kirill Kulakov <kirill578@gmail.com>
2014-02-19 21:07:07 +0200

    writing command from the end, and refactoring

--- misc/crt_zombies_address_to_stack.asm
+++ misc/crt_zombies_address_to_stack.asm
@@ -5,20 +5,20 @@ xor ax,ax
 
 @lp:
 mov [0c0deh + si],254
-xor ax,ax
-xor ax,ax
-xor ax,ax
-xor ax,ax
-xor ax,ax  
+xor ax,ax ; wait
+xor ax,ax ; wait
+xor ax,ax ; wait
+xor ax,ax ; wait
+xor ax,ax ; wait  
 
 mov [0c0deh + si],255 ;get the zombi moving
 mov al,[0c0ddh + si]
-mov bl,255 ; first can overflow
+mov bl,255 ; first cannot overflow
 mul bl
 mov cx,ax
 
-xor ax,ax
-xor ax,ax
+xor ax,ax ; wait
+xor ax,ax ; wait
 
 xor ax,ax
 mov al,[0c0ddh + si]
@@ -31,25 +31,15 @@ div bx ; orginal number is in dx
 
 mov di,dx
 
-mov word [di +17h],34eah
+mov word [di +17h +4],0010h ; write backward
 mov word [di +17h +2],0012h
-mov word [di +17h +4],0010h
+mov word [di +17h],34eah
 
-mov word [di + 8000h +17h],34eah
+mov word [di + 8000h +17h +4],0010h ; write backward
 mov word [di + 8000h +17h +2],0012h
-mov word [di + 8000h +17h +4],0010h
+mov word [di + 8000h +17h],34eah
 
-xor ax,ax
-
-;push dx ; store zombi
-
-;xor ax,ax
-;xor ax,ax
-;xor ax,ax
-;xor ax,ax
-;xor ax,ax
-;xor ax,ax
-;xor ax,ax
+xor ax,ax ; wait
 
 add si,100h
 cmp si,600h
