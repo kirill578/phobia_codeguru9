d823b4610e1abc1d3e044837f080a19a406ce8fa
Kirill Kulakov <kirill578@gmail.com>
2014-02-20 20:19:53 +0200

    all zombies code added to the end of our code, decreces the probobility of opponents to catch a zombie with int 87h from 100% to 33%

--- phobia2.asm
+++ phobia2.asm
@@ -144,4 +144,84 @@ call dx ; jmp and push
 @kill_self: 
 int 3
 @end:
-@end_zombi:
+@end_zombi:     
+
+
+; all zombie code bellow
+; wasting others int 87h
+; they will find this, and not the zombi
+
+;push cs
+;pop es
+;int 87h ; too far to be useful with int 87h
+and ax,07fffh
+@looop0:
+push ax
+mov bl,[0c0deh]
+test bl,bl
+jns @skip0:
+div bl
+mov [0c0ddh],ah
+@skip0:
+pop ax
+jmp @looop0:
+
+and ax,07fffh
+@looop1:
+push ax
+mov bl,[0c1deh]
+test bl,bl
+jns @skip1:
+div bl
+mov [0c1ddh],ah
+@skip1:
+pop ax
+jmp @looop1:
+
+and ax,07fffh
+@looop2:
+push ax
+mov bl,[0c2deh]
+test bl,bl
+jns @skip2:
+div bl
+mov [0c2ddh],ah
+@skip2:
+pop ax
+jmp @looop2:
+
+and ax,07fffh
+@looop3:
+push ax
+mov bl,[0c3deh]
+test bl,bl
+jns @skip3:
+div bl
+mov [0c3ddh],ah
+@skip3:
+pop ax
+jmp @looop3:
+
+and ax,07fffh
+@looop4:
+push ax
+mov bl,[0c4deh]
+test bl,bl
+jns @skip4:
+div bl
+mov [0c4ddh],ah
+@skip4:
+pop ax
+jmp @looop4:
+
+and ax,07fffh
+@looop5:
+push ax
+mov bl,[0c5deh]
+test bl,bl
+jns @skip5:
+div bl
+mov [0c5ddh],ah
+@skip5:
+pop ax
+jmp @looop5:
